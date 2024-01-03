e90ae2ca8166a68829d2da414c567118d99cf7b5
Edward Vincent Cuevas <edwardvincentcuevas7@gmail.com>
2024-01-03 10:51:14 +0800

    Initialize sveltekit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#164863",
+                secondary: "#427D9D",
+                tertiary: "#9BBEC8",
+                quaternary: "#DDF2FD",
+                light: "gray",
+                success: "#2DD36F",
+                warning: "#FFC409",
+                failure: "#EB445A",
+            },
+        },
+    },
+    plugins: [],
+};
